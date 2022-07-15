2c08ad0ce7b35ba1f8fd4fec02a472e9a2b34081
EnzoRafaelPassos <enzorafel14@gmail.com>
2022-07-15 16:03:52 -0300

    Meu portfolio em andamneto

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const animationClass = 'animate';
 
 function animeScroll() {
     const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-    target.forEach(function(element) {
+    target.forEach(function (element) {
         if ((windowTop) > element.offsetTop) {
             element.classList.add(animationClass);
         } else {
@@ -17,7 +17,21 @@ function animeScroll() {
 animeScroll();
 
 if (target.length) {
-    window.addEventListener('scroll', function() {
+    window.addEventListener('scroll', function () {
         animeScroll();
     })
 }
+
+
+// animaçao de titulo digitando 
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra , 75 * i );
+    });
+}
+
+const titulo = document.querySelector('.c-left > h1 ');
+typeWriter(titulo);
